c4a44c89380591cd7350e54bbc864f32c0a9d2a7
XinyuXie <soxuxiee@rtx8000-3.de>
2024-01-12 13:37:38 +0000

    add argparse in the main file

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from time import time
 from tqdm import tqdm
 import os
 import wandb
+import argparse
 
 from module.transformer import Transformer
 from module.loss import Myloss
@@ -12,22 +13,25 @@ from train import training_validation
 from plot import plot_learning_curve
 from evaluation import evaluation
 from utils.random_seed import setup_seed
-
-def main():
+setup_seed(30)
+def main(args):
     # 1. data preprocessing
     path = 'EEG_Eye_State_Classification.csv'
+    plot_folder_dir="./pic" 
+    model_folder_dir="./saved_models" 
 
     draw_key = 1  # 大于等于draw_key才会保存图像
     file_name = path.split('/')[-1][0:path.split('/')[-1].index('.')]  # 获得文件名字
     # 超参数设置
-    EPOCH = 10
-    BATCH_SIZE = 50
+    EPOCH = 100
+    BATCH_SIZE = 200
     LR = 1e-4
     DEVICE = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")  # 选择设备 CPU or GPU
+    patience = 10
     print(f'use device: {DEVICE}')
 
-    train_percentage = 0.6
-    validate_percentage = 0.2
+    train_percentage = 0.7
+    validate_percentage = 0.1
 
     d_model = 512
     d_hidden = 1024
@@ -38,7 +42,7 @@ def main():
     dropout = 0.2
     pe = True  # # 设置的是双塔中 score=pe score=channel默认没有pe
     mask = True  # 设置的是双塔中 score=input的mask score=channel默认没有mask
-    model_folder_dir="./saved_models" 
+    
     # 优化器选择
     optimizer_name = 'Adagrad'
 
@@ -95,17 +99,37 @@ def main():
     #     print("round"+str(exp_idx+1)+" has been done")
     model = Transformer(d_model=d_model, d_input=d_input, d_channel=d_channel, d_output=d_output, d_hidden=d_hidden,
                     q=q, v=v, h=h, N=N, dropout=dropout, pe=pe, mask=mask, device=DEVICE).to(DEVICE)
-    training_validation(model=model, epoch_sum=EPOCH, train_loader=train_loader, val_loader=val_loader, learning_rate=LR, patience=7, exp_index=1, model_folder_directory=model_folder_dir, DEVICE=DEVICE)
+    model_name, model, all_epoch_train_loss, all_epoch_val_loss=training_validation(model=model, epoch_sum=EPOCH, train_loader=train_loader, val_loader=val_loader, learning_rate=LR, patience=patience, exp_index=1, model_folder_directory=model_folder_dir, DEVICE=DEVICE)
+    plot_learning_curve(train_loss=all_epoch_train_loss,val_loss=all_epoch_val_loss,plot_folder_dir=plot_folder_dir,model_name=model_name)
     evaluation(model=model, dataloader=train_loader, DEVICE=DEVICE, flag = 'train_set')
-    evaluation(model=model, dataloader  =val_loader, DEVICE=DEVICE, flag='validation set')
+    evaluation(model=model, dataloader=val_loader, DEVICE=DEVICE, flag='validation set')
     
 
     # 3. evaluation test sets with accuracy, precision, F1 score and AUC
-
+    evaluation(model=model, dataloader=test_loader, DEVICE=DEVICE, flag='test set')
 
     # 4. plot
         
 
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser('Set hyper parameters for the training.')
+    parser.add_argument('--path',type=str, required=True, help="the path of the dataset")
+    parser.add_argument('--plot_folder_dir', type=str,required=True,help="the folder directory where plot results are stored")
+    parser.add_argument('--model_folder_dir', type=str, required=True,help="the directory of the model folder")
+    parser.add_argument('--EPOCH',type=int, required=True, default=100, help="the number of epochs")
+    parser.add_argument('--BATCH_SIZE', type=int, required=True, help="batch size")
+    parser.add_argument('--learning_rate', type=float, default=1e-4, required=True,help='the learning rate')
+    parser.add_argument('--patience', type=int, required=True, help='the counter of the early_stopping')
+    parser.add_argument('--train_percentage',type=float, required=True,help="the training percentage of the model")
+    parser.add_argument('--validate_percentage',type=float,required=True,help="the valiation set percentage")
+    parser.add_argument('--d_model', type=int, required=True, default=512, help="the dimension of the model")
+    parser.add_argument('--d_hidden', type=int, required=True, default=1024, help="The dimmension of the hidden layers in Position-wise Feedforward network")
+    parser.add_argument('-q', type=int, required=True, help='the dimension of the linear layer in the Multi-Head Attention')
+    parser.add_argument('-v', type=int, required=True, help='the dimension of the linear layer in the Multi-Head Attention')
+    parser.add_argument('-h', type=int, required=True, help='the head number of the Multi-Head Attention')
+    parser.add_argument('-N',  type=int, required=True, help='the number of the encoders')
+    parser.add_argument('--dropout', type=float, required=True, help="the random dropout")
+    parser.add_argument('--optimizer_name',type=str, required=True, default='Adagrad', help="The name of the optimizer")
+    args = parser.parse_args()
+    main(args=args)
