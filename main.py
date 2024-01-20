6f430441e42485a396de66886469f331a1794db3
XinyuXie <soxuxiee@rtx8000-3.de>
2024-01-20 17:32:24 +0000

    add config of wandb on main.py

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ from train import training_validation
 from plot import plot_learning_curve
 from evaluation import evaluation
 from utils.random_seed import setup_seed
-setup_seed(30)
+# setup_seed(30)
 def main(args):
     # 1. data preprocessing
     path = args.path
@@ -46,6 +46,7 @@ def main(args):
 
     # training_hyperparameters
     sliding_window_length = args.sliding_window_length
+    num_exps = args.num_exps
     
     # 优化器选择
     optimizer_name = args.optimizer_name
@@ -79,34 +80,35 @@ def main(args):
 
     print("===================train and validation====================")
     # experiments loop
-    # for exp_idx in range(args.num_exps):
-    #         # wandb initialization
-    #     wandb.init(project='GTNforEEG',
-    #             job_type="training",
-    #             reinit=True,
-    #             )
-    #     # model initialization
-    #     model = Transformer(d_model=d_model, d_input=d_input, d_channel=d_channel, d_output=d_output, d_hidden=d_hidden,
-    #             q=q, v=v, h=h, N=N, dropout=dropout, pe=pe, mask=mask, device=DEVICE).to(DEVICE)
-    #     wandb.watch(model,log="all")
-    #     model_name, model, all_epoch_train_loss, all_epoch_val_loss = training_validation(model=model,
-    #                                                     epoch_sum=args.num_of_epochs,
-    #                                                     train_loader=train_loader,
-    #                                                     val_loader=val_loader,
-    #                                                     patience=args.patience,
-    #                                                     learning_rate=args.learning_rate,
-    #                                                     exp_index=exp_idx,
-    #                                                     model_folder_directory=args.model_folder_dir)
-    #     # plot the learning curve
-    #     plot_learning_curve(train_loss=all_epoch_train_loss,val_loss=all_epoch_val_loss,plot_folder_dir=args.plot_folder_dir,model_name=model_name)
-
-    #     print("round"+str(exp_idx+1)+" has been done")
-    model = Transformer(d_model=d_model, d_input=d_input, d_channel=d_channel, d_output=d_output, d_hidden=d_hidden,
+    for exp_idx in range(num_exps):
+            # wandb initialization
+        config = dict(learningRate = LR, batch_size = BATCH_SIZE, num_of_epochs = EPOCH,
+                      sliding_window_length=sliding_window_length,num_of_experiments = num_exps,
+                      optimizer = optimizer_name, head_of_multi_attention=h)
+        wandb.init(project='GTNforEEG',
+                job_type="training",
+                config=config,
+                reinit=True,
+                )
+        # model initialization
+        model = Transformer(d_model=d_model, d_input=d_input, d_channel=d_channel, d_output=d_output, d_hidden=d_hidden,
                     q=q, v=v, h=h, N=N, dropout=dropout, pe=pe, mask=mask, device=DEVICE).to(DEVICE)
-    model_name, model, all_epoch_train_loss, all_epoch_val_loss=training_validation(model=model, epoch_sum=EPOCH, train_loader=train_loader, val_loader=val_loader, learning_rate=LR, patience=patience, exp_index=1, model_folder_directory=model_folder_dir, DEVICE=DEVICE,optimizer_name=optimizer_name)
-    plot_learning_curve(train_loss=all_epoch_train_loss,val_loss=all_epoch_val_loss,plot_folder_dir=plot_folder_dir,model_name=model_name)
-    evaluation(model=model, dataloader=train_loader, DEVICE=DEVICE, flag = 'train_set')
-    evaluation(model=model, dataloader=val_loader, DEVICE=DEVICE, flag='validation set')
+        wandb.watch(model,log="all")
+        model_name, model, all_epoch_train_loss, all_epoch_val_loss=training_validation(model=model, epoch_sum=EPOCH, train_loader=train_loader, 
+                                                                                        val_loader=val_loader, test_loader=test_loader, learning_rate=LR, patience=patience, exp_index=1, 
+                                                                                        model_folder_directory=model_folder_dir, DEVICE=DEVICE,optimizer_name=optimizer_name)
+    
+        # plot the learning curve
+        plot_learning_curve(train_loss=all_epoch_train_loss,val_loss=all_epoch_val_loss,plot_folder_dir=plot_folder_dir,model_name=model_name)
+
+        print("round"+str(exp_idx+1)+" has been done")
+    # model = Transformer(d_model=d_model, d_input=d_input, d_channel=d_channel, d_output=d_output, d_hidden=d_hidden,
+    #                 q=q, v=v, h=h, N=N, dropout=dropout, pe=pe, mask=mask, device=DEVICE).to(DEVICE)
+    # model_name, model, all_epoch_train_loss, all_epoch_val_loss=training_validation(model=model, epoch_sum=EPOCH, train_loader=train_loader, val_loader=val_loader, learning_rate=LR, patience=patience, exp_index=1, model_folder_directory=model_folder_dir, DEVICE=DEVICE,optimizer_name=optimizer_name)
+    # plot_learning_curve(train_loss=all_epoch_train_loss,val_loss=all_epoch_val_loss,plot_folder_dir=plot_folder_dir,model_name=model_name)
+
+    # evaluation(model=model, dataloader=train_loader, DEVICE=DEVICE, flag = 'train_set')
+    # evaluation(model=model, dataloader=val_loader, DEVICE=DEVICE, flag='validation set')
     
 
     # 3. evaluation test sets with accuracy, precision, F1 score and AUC
@@ -136,5 +138,6 @@ if __name__ == "__main__":
     parser.add_argument('--dropout', type=float, required=True, help="the random dropout")
     parser.add_argument('--sliding_window_length', type=int, required=True, help="The length of sliding window when spliting the data")
     parser.add_argument('--optimizer_name',type=str, required=True, default='Adagrad', help="The name of the optimizer")
+    parser.add_argument('--num_exps', type=int, required=True, help="The quantity of the experiments")
     args = parser.parse_args()
     main(args=args)
