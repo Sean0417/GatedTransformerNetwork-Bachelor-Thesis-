f5128c7959dfc1e6d1ff9b30a29e79d3c6440358
XinyuXie <soxuxiee@rtx8000-3.de>
2024-01-22 17:40:35 +0000

    add BCEloss function

--- module/loss.py
+++ module/loss.py
@@ -1,11 +1,13 @@
 from torch.nn import Module
 import torch
 from torch.nn import CrossEntropyLoss
+from torch.nn import BCELoss
 
 class Myloss(Module):
     def __init__(self):
         super(Myloss, self).__init__()
-        self.loss_function = CrossEntropyLoss()
+        # self.loss_function = CrossEntropyLoss()
+        self.loss_function = BCELoss()
 
     def forward(self, y_pre, y_true):
         y_true = y_true.long()
