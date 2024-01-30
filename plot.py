1520a66788eadd1445a67d6e35c4121768d910c1
XinyuXie <soxuxiee@rtx8000-3.de>
2024-01-30 10:24:05 +0000

    add plot confusion matrix

--- plot.py
+++ plot.py
@@ -3,9 +3,11 @@ import numpy as np
 import os
 import wandb
 from sklearn.metrics import confusion_matrix
+import torch.nn.functional as F
 import matplotlib.pyplot as plt
 import seaborn as sns
 import time
+import torch
 def plot_learning_curve(train_loss, val_loss, plot_folder_dir, model_name):
     # visualize the loss as the network trained
     plt.figure()
@@ -67,7 +69,7 @@ def plot_Confusion_Matrix(y_true, y_pred, file_name, flag="test_set"):
     plt.xlabel('Predicted')
     plt.ylabel('True')
 
-    plot_folder_dir = file_name+ "_confusion_matrix/"
+    plot_folder_dir = 'Confusion_Matrix/'+file_name+ "_confusion_matrix/"
 
     plot_time = time.strftime("%Y%m%d_%H%M%S")
     if os.path.exists(plot_folder_dir):
@@ -75,3 +77,47 @@ def plot_Confusion_Matrix(y_true, y_pred, file_name, flag="test_set"):
     else:
         os.makedirs(plot_folder_dir)
         plt.savefig(plot_folder_dir+'/'+flag+"_Confusion_Matrix_"+plot_time+'.png',format='png',dpi= 200)
+
+def plot_heat_map(dataloader,model,file_name,DEVICE):
+    with torch.no_grad():
+        model.eval()
+        for x, y in dataloader:
+            x, y = x.to(DEVICE), y.to(DEVICE)
+            y_pre, _, score_input, score_channel, _, _, _ = model(x, 'test') # y_pre is a tensor with a dimension of batchsize*2(200*2 for instance if the batchsize is 200),
+            break
+    plot_time = time.strftime("%Y%m%d_%H%M%S")
+    score_input = score_input.detach().cpu().numpy()
+    score_channel = score_channel.detach().cpu().numpy()
+
+    # plot score_input
+    fig_input, axes_input = plt.subplots(4, score_input.shape[0]/4, figsize=(20, 20))
+    for i in range(score_input.shape[0]):
+        ax = axes_input[i]
+        sns.heatmap(score_input[i], ax=ax, cmap='Blues')
+        ax.set_title(f'Stepwise Attention Heatmap on Head {i+1}')
+        ax.set_xlabel('Key')
+        ax.set_ylabel('Query')
+    folder_name = "Heat_Map/heatmap_score_input"+file_name
+    plt.tight_layout()
+    if os.path.exists(folder_name):
+        plt.savefig(folder_name+'/'+"Heatmap_"+plot_time+'.png',format='png',dpi= 200)
+    else:
+        os.makedirs(folder_name)
+        plt.savefig(folder_name+'/'+"Heatmap_"+plot_time+'.png',format='png',dpi= 200)
+    # plot score_channel
+    fig_channel, axes_channel = plt.subplots(1, score_channel.shape[0], figsize=(20, 5))
+    for i in range(score_channel.shape[0]):
+        ax = axes_channel[i]
+        sns.heatmap(score_channel[i], ax=ax, cmap='Blues')
+        ax.set_title(f'Channelwise Attention Heatmap on Head {i+1}')
+        ax.set_xlabel('Key')
+        ax.set_ylabel('Query')
+    folder_name = "Heat_Map/heatmap_score_channel"+file_name
+    if os.path.exists(folder_name):
+        plt.savefig(folder_name+'/'+"Heatmap_"+plot_time+'.png',format='png',dpi= 200)
+    else:
+        os.makedirs(folder_name)
+        plt.savefig(folder_name+'/'+"Heatmap_"+plot_time+'.png',format='png',dpi= 200)
+
+
+    
