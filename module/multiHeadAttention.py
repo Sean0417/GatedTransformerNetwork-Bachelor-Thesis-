23637f7f4373cdf1b25cb44f22ba00b0fd3bed3b
XinyuXie <soxuxiee@rtx8000-3.de>
2024-01-31 00:07:20 +0000

    change the score

--- module/multiHeadAttention.py
+++ module/multiHeadAttention.py
@@ -27,7 +27,7 @@ class MultiHeadAttention(Module):
 
         self.mask = mask
         self.dropout = torch.nn.Dropout(p=dropout)
-        self.score = None
+        
 
     def forward(self, x, stage):
         Q = torch.cat(self.W_q(x).chunk(self._h, dim=-1), dim=0)
@@ -35,7 +35,7 @@ class MultiHeadAttention(Module):
         V = torch.cat(self.W_v(x).chunk(self._h, dim=-1), dim=0)
 
         score = torch.matmul(Q, K.transpose(-1, -2)) / math.sqrt(self._q)
-        self.score = score
+        
 
         if self.mask and stage == 'train':
             mask = torch.ones_like(score[0])
@@ -43,6 +43,8 @@ class MultiHeadAttention(Module):
             score = torch.where(mask > 0, score, torch.Tensor([-2**32+1]).expand_as(score[0]).to(self.device))
 
         score = F.softmax(score, dim=-1)
+
+        self.score = score
 
         attention = torch.matmul(score, V)
 
